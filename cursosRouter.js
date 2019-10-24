43d31e0c2f3c480cc8f7022cafa070a3ba4e0a57
mafeZapata <49245929+mafeZapata@users.noreply.github.com>
2019-10-23 20:47:31 -0500

    Add files via upload

--- cursosRouter.js
+++ cursosRouter.js
@@ -7,41 +7,72 @@ const misCursos = new CursoController()
 // ENRUTADO DE cursos
 router.get('/cursos/', (request, response) => {
     let respuesta = {}
-    respuesta.estado = true
-    respuesta.informacion = misCursos.obtenerCursos()
+    respuesta.informacion = misCursos.obtenerCursos().then(()=>{
+      respuesta.estado = true
     respuesta.mensaje = "el curso ha sido consultado"
-    response.send(respuesta)
+    response.send(respuesta)  
+    }).catch((error)=>{
+        respuesta.estado = false
+        respuesta.mensaje = "el curso NO ha sido consultado"
+        response.send(error)  
+    })
+    
 })
 
 router.post('/cursos/', (request, response) => {
     let curso = request.body
-    misCursos.agregarCursos(curso)
-    let respuesta = {}
+    misCursos.agregarCursos(curso).then(()=>{
+ let respuesta = {}
     respuesta.estado = true
     respuesta.informacion = curso
     respuesta.mensaje = "el curso ha sido agregado"
     response.send(respuesta)
+    }).catch((eror)=>{
+        let respuesta = {}
+    respuesta.estado = false
+    respuesta.informacion = curso
+    respuesta.mensaje = "el curso  NO ha sido agregado"
+    response.send(error)
+    })
+   
 })
 
 router.delete('/cursos/:id', (request, response) => {
     let id = request.params.id
-    misCursos.eliminarCursos(id)
-    let respuesta = {}
+    misCursos.eliminarCursos(id).then(()=>{
+        let respuesta = {}
     respuesta.estado = true
     respuesta.informacion = {}
     respuesta.mensaje = "El curso se ha eliminado correctamente"
-    response.send(respuesta)
+    response.send(respuesta) 
+    }).catch((error)=>{
+        let respuesta = {}
+        respuesta.estado = false
+        respuesta.informacion = {}
+        respuesta.mensaje = "El curso  NO se ha eliminado correctamente"
+        response.send(error) 
+        
+    })
+   
 })
 
 router.put('/cursos/:id', (request, response) => {
     let id = request.params.id
     let curso = request.body
-    misCursos.modificarCursos(id, curso)
-    let respuesta = {}
+    misCursos.modificarCursos(id, curso).then(()=>{
+      let respuesta = {}
     respuesta.estado = true
     respuesta.informacion = {}
     respuesta.mensaje = "el curso ha sido modificado con exito"
-    response.send(respuesta)
+    response.send(respuesta)  
+    }).catch((error)=>{
+        let respuesta = {}
+        respuesta.estado = false
+        respuesta.informacion = {}
+        respuesta.mensaje = "el curso NO ha sido modificado con exito"
+        response.send(error)  
+    })
+    
 })
 
 module.exports = router
--- estudiantesRouter.js
+++ estudiantesRouter.js
@@ -8,40 +8,70 @@ const misEstudiantes = new EstudiantesController()
 router.get('/estudiantes/', (request, response) => {
     let respuesta = {}
     respuesta.estado = true
-    respuesta.informacion = misEstudiantes.obtenerEstudiantes()
-    respuesta.mensaje = "Estudiantes consutados"
+    respuesta.informacion = misEstudiantes.obtenerEstudiantes().then(()=>{
+respuesta.mensaje = "Estudiantes consutados"
     response.send(respuesta)
+    }).catch((error)=>{
+        respuesta.mensaje = "Estudiantes NO consutados"
+        response.send(error)
+    })
+    
 })
 
 router.post('/estudiantes/', (request, response) => {
     let estudiante = request.body
     misEstudiantes.agregarEstudiantes(estudiante)
-    let respuesta = {}
-    respuesta.estado = true
+    .then(()=>{
+        let respuesta = {}
+        respuesta.estado = true
+        respuesta.informacion = estudiante
+        respuesta.mensaje = "El estudiante ha sido agregado"
+        response.send(respuesta)
+    }).catch((error)=>{
+let respuesta = {}
+    respuesta.estado = false
     respuesta.informacion = estudiante
-    respuesta.mensaje = "El estudiante ha sido agregado"
-    response.send(respuesta)
+    respuesta.mensaje = "El estudiante NO ha sido agregado"
+    response.send(error)
+    })
+    
 })
 
 router.delete('/estudiantes/:id', (request, response) => {
     let id = request.params.id
-    misEstudiantes.eliminarEstudiantes(id)
-    let respuesta = {}
+    misEstudiantes.eliminarEstudiantes(id).then(()=>{
+ let respuesta = {}
     respuesta.estado = true
     respuesta.informacion = {}
     respuesta.mensaje = "El estudiante se ha eliminado correctamente"
     response.send(respuesta)
+    }).catch((error)=>{
+        let respuesta = {}
+        respuesta.estado = false
+        respuesta.informacion = {}
+        respuesta.mensaje = "El estudiante  NO se ha eliminado correctamente"
+        response.send(error)
+    })
+   
 })
 
 router.put('/estudiantes/:id', (request, response) => {
     let id = request.params.id
     let estudiante = request.body
-    misEstudiantes.modificarEstudiantes(id, tarea)
-    let respuesta = {}
+    misEstudiantes.modificarEstudiantes(id, tarea).then(()=>{
+let respuesta = {}
     respuesta.estado = true
     respuesta.informacion = {}
     respuesta.mensaje = "El estudiante ha sido modificado"
     response.send(respuesta)
+    }).catch((error)=>{
+        let respuesta = {}
+        respuesta.estado = false
+        respuesta.informacion = {}
+        respuesta.mensaje = "El estudiante NO ha sido modificado"
+        response.send(error)
+    })
+    
 })
 
 module.exports = router
